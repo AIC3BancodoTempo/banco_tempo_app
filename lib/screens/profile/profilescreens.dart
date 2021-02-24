ce4e2b914e523a043d3e1e991301e0f02dfe0fe7
igorpenhas <73540937+igorpenhas@users.noreply.github.com>
2021-02-24 20:46:51 -0300

    fix: componetização da tela

--- lib/screens/profile/profilescreens.dart
+++ lib/screens/profile/profilescreens.dart
@@ -1,7 +1,7 @@
+import 'components/header.dart';
 import 'package:flutter/material.dart';
-import 'dart:io';
-import 'dart:async';
-import 'package:image_picker/image_picker.dart';
+import 'components/button.dart';
+import 'components/textfield.dart';
 
 class Profilescreen extends StatefulWidget {
   Profilescreen({Key key, this.title}) : super(key: key);
@@ -13,180 +13,46 @@ class Profilescreen extends StatefulWidget {
 
 class _ProfilescreenState extends State<Profilescreen> {
   @override
-  File imageFile;
-
-  _openGallery(BuildContext context) async {
-    var picture = await ImagePicker.pickImage(source: ImageSource.gallery);
-    this.setState(() {
-      imageFile = picture;
-    });
-    Navigator.of(context).pop();
-  }
-
-  _openCamera(BuildContext context) async {
-    var picture = await ImagePicker.pickImage(source: ImageSource.camera);
-    this.setState(() {
-      imageFile = picture;
-    });
-    Navigator.of(context).pop();
-  }
-
-  Future<void> _showChoiceDialog(BuildContext context) {
-    return showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-              title: Text("Imagem"),
-              content: SingleChildScrollView(
-                  child: ListBody(
-                children: <Widget>[
-                  GestureDetector(
-                    child: Text("Dados"),
-                    onTap: () {
-                      _openGallery(context);
-                    },
-                  ),
-                  Padding(padding: EdgeInsets.all(8.0)),
-                  GestureDetector(
-                    child: Text("Câmera"),
-                    onTap: () {
-                      _openCamera(context);
-                    },
-                  ),
-                ],
-              )));
-        });
-  }
-
-  @override
-  Widget _decideImageView() {
-    // IMAGEM
-    if (imageFile == null) {
-      //se a imagem for nula
-      return Container(
-        padding: EdgeInsets.fromLTRB(0, 20, 0, 10),
-        width: 100,
-        height: 80,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: Colors.grey[300],
-        ),
-      );
-    } else {
-      return Container(
-        child: Image.file(
-          imageFile,
-          width: 250,
-          height: 220,
-        ),
-        padding: EdgeInsets.fromLTRB(0, 20, 0, 10),
-        width: 150,
-        height: 150,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: Colors.grey[300],
-        ),
-      );
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
         children: <Widget>[
-          Container(
-              decoration: BoxDecoration(
-                color: Colors.black,
-                // imagem de fundo não consegui colocar a imagem de fundo deixei preto a imabem tá em assets/img
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(16),
-                  bottomRight: Radius.circular(16),
-                ),
-              ),
-              child: Column(
-                children: <Widget>[
-                  SizedBox(height: 30),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Column(
-                        children: <Widget>[
-                          _decideImageView(),
-                        ],
-                      )
-                    ],
-                  ),
-                  SizedBox(height: 22),
-                  Text(
-                    'Nome do Contato',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  Text(
-                    '12.5 Horas Acumuladas',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  SizedBox(height: 10),
-                  IconButton(
-                    color: Colors.white,
-                    icon: Icon(Icons.camera_alt),
-                    onPressed: () {
-                      _showChoiceDialog(context);
-                    },
-                  ),
-                ],
-              )),
+          ProfileHeader(),
           SizedBox(height: 5),
           Container(
             padding: EdgeInsets.fromLTRB(2, 20, 2, 15),
             child: Column(
               children: <Widget>[
-                ListTile(
-                    leading: Icon(Icons.email, color: Colors.black),
-                    title: TextField(
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                          labelText: "Nome de Usuário",
-                          labelStyle: TextStyle(color: Colors.black)),
-                      style: TextStyle(color: Colors.black, fontSize: 15.0),
-                    )),
-                ListTile(
-                  leading: Icon(Icons.person, color: Colors.black),
-                  title: TextField(
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                        labelText: "E-Mail",
-                        labelStyle: TextStyle(color: Colors.black)),
-                    style: TextStyle(color: Colors.black, fontSize: 15.0),
-                  ),
+                ProfileTextField(
+                  inputType: TextInputType.text,
+                  label: "Nome de Usuário",
+                  onChanged: (value) {
+                    print(value);
+                  },
+                  leadingIcon: Icons.person,
                 ),
-                ListTile(
-                    leading: Icon(Icons.lock, color: Colors.black),
-                    title: TextField(
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                          labelText: "Senha",
-                          labelStyle: TextStyle(color: Colors.black)),
-                      style: TextStyle(color: Colors.black, fontSize: 15.0),
-                    )),
+                ProfileTextField(
+                  inputType: TextInputType.emailAddress,
+                  label: "E-mail",
+                  onChanged: (value) {
+                    print(value);
+                  },
+                  leadingIcon: Icons.mail,
+                ),
+                ProfileTextField(
+                  inputType: TextInputType.text,
+                  label: "Senha",
+                  onChanged: (value) {
+                    print(value);
+                  },
+                  leadingIcon: Icons.lock,
+                ),
               ],
             ),
           ),
-          FlatButton(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(18.0),
-                side: BorderSide(color: Colors.black)),
-            color: Colors.white,
-            textColor: Colors.black,
-            padding: EdgeInsets.all(8.0),
+          ProfileButton(
             onPressed: () {},
-            child: Text(
-              "Atualizar".toUpperCase(),
-              style: TextStyle(
-                fontSize: 14.0,
-              ),
-            ),
-          )
+          ),
         ],
       ),
     );
