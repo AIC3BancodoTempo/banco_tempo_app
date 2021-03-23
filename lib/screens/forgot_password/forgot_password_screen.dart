08cd2afa970629dc7fa84a74952d4209542990d2
Matheus Scheunemann <matheuss.sls@gmail.com>
2021-03-23 16:08:56 -0300

    feat: Inclusão da forgot screen

---
+++ lib/screens/forgot_password/forgot_password_screen.dart
@@ -0,0 +1,85 @@
+import 'package:banco_do_tempo_app/screens/login/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../blocs/auth/auth_bloc.dart';
+import '../core/already_have_an_account_acheck.dart';
+import '../core/rounded_button.dart';
+import '../core/rounded_input.dart';
+
+class ForgotPasswordScreen extends StatelessWidget {
+  final AuthBloc authBloc;
+
+  const ForgotPasswordScreen({Key key, @required this.authBloc})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        height: size.height,
+        width: double.infinity,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Positioned(
+              top: 0,
+              left: 0,
+              child: Image.asset(
+                "assets/images/signup_top.png",
+                width: size.width * 0.35,
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              child: Image.asset(
+                "assets/images/main_bottom.png",
+                width: size.width * 0.25,
+              ),
+            ),
+            SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "RECUPERAR SENHA",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: size.height * 0.03),
+                  SvgPicture.asset(
+                    "assets/icons/signup.svg",
+                    height: size.height * 0.35,
+                  ),
+                  RoundedInput(
+                    hintText: "Seu e-mail",
+                    onChanged: (value) {},
+                    icon: Icons.mail,
+                  ),
+                  RoundedButton(
+                    text: "RECEBER INSTRUÇÕES",
+                    press: () {},
+                  ),
+                  SizedBox(height: size.height * 0.03),
+                  AlreadyHaveAnAccountCheck(
+                    login: false,
+                    press: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => LoginScreen(
+                            authBloc: authBloc,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
