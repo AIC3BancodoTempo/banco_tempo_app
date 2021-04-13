679b93f58a1cd016a21d0da66006e986f864672d
Joelson Sartori <joelsonsartori@gmail.com>
2021-04-13 18:48:14 -0300

    feat: finalização produc model

--- lib/core/models/produto_model.dart
+++ lib/core/models/produto_model.dart
@@ -11,7 +11,7 @@ class ProdutoModel {
   int status;
   String data;
   List<String> images;
-
+  QueryDocumentSnapshot documentSnapshot;
   ProdutoModel(
       {this.userPostId,
       this.userPostName,
@@ -22,7 +22,33 @@ class ProdutoModel {
       this.productDescritpion,
       this.status,
       this.images,
-      this.custoHoras});
+      this.custoHoras,
+      this.documentSnapshot});
+
+  ProdutoModel copyWith({
+    String userPostId,
+    String userPostName,
+    String productId,
+    String productName,
+    String productDescritpion,
+    int productQuantity,
+    double custoHoras,
+    int status,
+    String data,
+    List<String> images,
+  }) {
+    return ProdutoModel(
+        userPostId: userPostId ?? this.userPostId,
+        userPostName: userPostName ?? this.userPostName,
+        productId: productId ?? this.productId,
+        productName: productName ?? this.productName,
+        productDescritpion: productDescritpion ?? this.productDescritpion,
+        productQuantity: productQuantity ?? this.productQuantity,
+        custoHoras: custoHoras ?? this.custoHoras,
+        status: status ?? this.status,
+        data: data ?? this.data,
+        images: images ?? this.images);
+  }
 
   ProdutoModel.fromSnapshot(QueryDocumentSnapshot documentSnapshot) {
     Map<String, dynamic> data = documentSnapshot.data();
@@ -37,5 +63,21 @@ class ProdutoModel {
     custoHoras = data['valor_em_horas'] != null ? data['valor_em_horas'] : 0;
     images =
         data['valor_em_horas'] != null ? List<String>.from(data['imagem']) : [];
+  }
+
+  factory ProdutoModel.fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+
+    return ProdutoModel(
+        userPostId: map['userPostId'],
+        userPostName: map['userPostName'],
+        productId: map['productId'],
+        productName: map['productName'],
+        productDescritpion: map['productDescritpion'],
+        productQuantity: map['productQuantity'],
+        custoHoras: map['custoHoras'],
+        status: map['status'],
+        data: map['data'],
+        images: map['images']);
   }
 }
