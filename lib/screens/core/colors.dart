aa08f6e3dda5d4f02c36bd0b75c28799d6694022
jardeldargas <jardeldargas@hotmail.com>
2021-05-03 15:55:49 -0300

    feat: launcher icon

--- lib/screens/core/colors.dart
+++ lib/screens/core/colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-final themeSimpleColor = Colors.deepPurple;
+final themeSimpleColor = Color(0xFF8D44FF);
 final chatMessageColor = Colors.grey;
 final chatAppBarColor = Colors.white70;
 const themeColor = Color(0xFF6F35A5);
